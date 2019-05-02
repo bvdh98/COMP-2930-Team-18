a5ef248188e63e2786cb1f959abf0c7208a037ee
spencerwilson123321 <spencerwilson123321@gmail.com>
2019-05-02 14:06:54 -0700

    changes

---
+++ js/firebaseinitialize.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAqKxtkDyuT3IKCrtXsEW7eWMG97DL6PFw",
+    authDomain: "comp2930-3639b.firebaseapp.com",
+    databaseURL: "https://comp2930-3639b.firebaseio.com",
+    projectId: "comp2930-3639b",
+    storageBucket: "comp2930-3639b.appspot.com",
+    messagingSenderId: "773056439844"
+  };
+  firebase.initializeApp(config);
