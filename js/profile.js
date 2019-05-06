61599505a9b6d81fa60758648b52cc5c9b391258
spencerwilson123321 <spencerwilson123321@gmail.com>
2019-05-06 11:25:50 -0700

    profile javascript changes

--- js/profile.js
+++ js/profile.js
@@ -3,30 +3,20 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     let name = document.getElementById("userid");
     name.append(user.displayName);
-      
-    /* Getting Database Reference */
-      
-    let dbRef = firebase.database();
-    
-    /* .ref specifies what key in the DB. Grabbing the value of key1 and appending to a span in the html document. */
-      var average = 0;
-      var sum = 0;
-      var currentNum = 0;
-      for (var count = 1; count < 5; count++) {
-      
-      dbRef.ref("UserID/" + user.uid + "/key" + count).on('value', function(snapshot){  
-        currentNum = snapshot.val();  
-      });
-          sum += currentNum;
-      }
-      
-      //average = sum / 4;
-      
-      document.getElementById("calcAverage").append(sum);
-      
   } else {
     console.log("spencer sucks LOL");
   }
-});
-    
+
+var query = firebase.database().ref("UserID/" + user.uid).orderByKey();
+let number = 0;
+    query.once("value")
+        .then(function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                var key = childSnapshot.key;
+                number += childSnapshot.val();
+            });
+    let average = number / 4;
+    document.getElementById("calcAverage").append(average);
+        });
+    });
 });
