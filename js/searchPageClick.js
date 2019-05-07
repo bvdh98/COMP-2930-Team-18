9aba36813297fcc1c4632cf95f2b384aae12b29d
royroyzhong <zhongroy189@gmail.com>
2019-05-07 10:34:51 -0700

    adding js

---
+++ js/searchPageClick.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+            //            $('#logo').on('click', function() {
+            //
+            //                $('#submit').addClass('displayNone');
+            //                $('#trysearchBarKeyWord').hide();
+            //
+            //
+            //            });
+            $('#trycamera').on('click', function() {
+
+                $('.uploadimg').show();
+                $('#trysearchBarKeyWord').hide();
+                $('#trysearch').hide();
+                $('#tryImg').addClass("forImageUpload");
+
+
+            });
+
+        });
