71975769d9f0106b5cc11d112532d8ecd51bf7f5
spencerwilson123321 <spencerwilson123321@gmail.com>
2019-05-07 18:32:06 -0700

    search page changes

--- js/searchpage.js
+++ js/searchpage.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-
+    
     $("#submit").on("click", function() {
         $("#detailsDropdown, #score, #listChoice, #detailsBox, #yes, #no").toggle();
+        
+    });
+    
+    $("#detailsDropdown").on("click", function(){
+
+       $("#row1, #row2, #row3, #row4").toggle(500);
+        
     });
     
 });
