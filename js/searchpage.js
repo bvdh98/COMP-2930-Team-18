d653a6d9f863063c413c09aea5575f503a05090c
spencerwilson123321 <spencerwilson123321@gmail.com>
2019-05-12 12:22:12 -0700

    changes

--- js/searchpage.js
+++ js/searchpage.js
@@ -95,47 +95,6 @@ $(document).ready(function() {
             $("#prodName").html("Lucky Charms");
         };
         
-        
-//        $.ajax({
-//            type: "POST",
-//            url: "/cornflakes",
-//            datatype: "jsonp",
-//            success: function(data) {
-//                $("#row1").html(data.row1);
-//                $("#row2").html(data.row2);
-//                $("#row3").html(data.row3);
-//                $("#row4").html(data.row4);
-//                $("#foodScore").html(data.score);
-//                console.log(data);
-//            }
-//        });
-//        $.ajax({
-//            type: "GET",
-//            url: "/search",
-//            datatype: "jsonp",
-//        }).done(function(data) {
-//                $("#row1").html(data.row1);
-//                $("#row2").html(data.row2);
-//                $("#row3").html(data.row3);
-//                $("#row4").html(data.row4);
-//                $("#foodScore").html(data.score);
-//                console.log(data);
-//            });
-        
-//        if (search == "Crispy Rice"){
-//        $.ajax({
-//            type: "GET",
-//            url: "/crispyrice"
-//        }).done(function(data) {
-//                $("#row1").append(data.row1);
-//                $("#row2").append(data.row2);
-//                $("#row3").append(data.row3);
-//                $("#row4").append(data.row4);
-//                $("#foodScore").append(data.score);
-//                console.log(data);
-//            });
-//        };
-        
     });
     
     $("#detailsDropdown").on("click", function(){
