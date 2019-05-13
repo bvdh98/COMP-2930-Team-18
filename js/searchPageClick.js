6dc6d860ddcc1307ec9792576ece1dc76a5cc92f
zhongRuming <Roy@zhongruingdembp.wifi.bcit.ca>
2019-05-13 14:20:19 -0700

    image upload with preview, show file name

--- js/searchPageClick.js
+++ js/searchPageClick.js
@@ -17,6 +17,19 @@ $(document).ready(function () {
 
         $('.uploadimg').prepend("<button id='iambtn' > Back </button>");
 
+        $("#imageUpload").fileinput({
+
+            previewFileType: "image",
+            browseClass: "btn btn-success",
+            browseLabel: "Pick Image",
+            browseIcon: "<i class=\"glyphicon glyphicon-picture\"></i> ",
+            removeClass: "btn btn-danger",
+            removeLabel: "Delete",
+            removeIcon: "<i class=\"glyphicon glyphicon-trash\"></i> ",
+            uploadClass: "btn btn-info",
+            uploadLabel: "Upload",
+            uploadIcon: "<i class=\"glyphicon glyphicon-upload\"></i> "
+        });
 
     });
 
